9b7fca9a99c9873a33969ccb83fdea99bcff39f9
Greg Taylor <greg@Gregs-Laptop.local>
2023-01-13 22:33:33 -0700

    test commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import '../styles/Header.css'
 function Header(){
     return(
         <div id='header'>
+            <h4>Pricing</h4>
             <h2>Choose the Right Plan For You</h2>
         </div>
     )
