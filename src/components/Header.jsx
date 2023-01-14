3ca62313afaa63048ce4e009e06b323669c147ba
Greg Taylor <greg@Gregs-Laptop.local>
2023-01-13 22:20:21 -0700

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import '../styles/Header.css'
+
+function Header(){
+    return(
+        <div id='header'>
+            <h2>Choose the Right Plan For You</h2>
+        </div>
+    )
+}
+
+export default Header
